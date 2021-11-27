416efa73ee5762b4ebf7cea00dd33d22116e98b3
Martin Chaparro <martin.a.chaparro@gmail.com>
2021-11-27 18:54:10 +0000

    basic control newGame form

---
+++ client/src/components/forms/newgame/FormNewgame.js
@@ -0,0 +1,181 @@
+import { useState } from 'react';
+import styles from './FormNewgame.module.css';
+
+export const FormNewgame = () => {
+	const [formValues, setFormValues] = useState({
+		name: '',
+		description: '',
+		released: '',
+		rating: '',
+		background_image: '',
+		genres: [],
+		platforms: [],
+	});
+
+	const { name, description, released, rating, background_image } = formValues;
+
+	const handleInputChange = ({ target }) => {
+		setFormValues({
+			...formValues,
+			[target.name]: target.value,
+		});
+	};
+
+	const handleCheckboxGenres = ({ target }) => {
+		if (target.checked) {
+			setFormValues({
+				...formValues,
+				genres: [...formValues.genres, parseInt(target.value)],
+			});
+		} else {
+			setFormValues({
+				...formValues,
+				genres: formValues.genres.filter(
+					(genreId) => genreId !== parseInt(target.value)
+				),
+			});
+		}
+	};
+
+	const handleCheckboxPlatforms = ({ target }) => {
+		if (target.checked) {
+			setFormValues({
+				...formValues,
+				platforms: [
+					...formValues.platforms,
+					{ id: parseInt(target.value), name: target.name },
+				],
+			});
+		} else {
+			setFormValues({
+				...formValues,
+				platforms: formValues.platforms.filter(
+					(platform) => platform.id !== parseInt(target.value)
+				),
+			});
+		}
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(formValues);
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<div className={styles.formBox}>
+				<div>
+					<label>Name: </label>
+					<input
+						type="text"
+						name="name"
+						value={name}
+						onChange={handleInputChange}
+					/>
+				</div>
+
+				<div>
+					<label>Description: </label>
+					<textarea
+						name="description"
+						value={description}
+						onChange={handleInputChange}
+					></textarea>
+				</div>
+				<div>
+					<label>Released: </label>
+					<input
+						type="date"
+						name="released"
+						value={released}
+						onChange={handleInputChange}
+					/>
+				</div>
+				<div>
+					<label>Rating: </label>
+					<input
+						type="number"
+						min="0"
+						max="5"
+						step="any"
+						name="rating"
+						value={rating}
+						onChange={handleInputChange}
+					/>
+				</div>
+				<div>
+					<label>
+						<input
+							type="checkbox"
+							value={1}
+							name="Aventuras"
+							onChange={handleCheckboxGenres}
+						/>
+						Aventuras
+					</label>
+					<label>
+						<input
+							type="checkbox"
+							value={2}
+							name="Rol"
+							onChange={handleCheckboxGenres}
+						/>
+						Rol
+					</label>
+					<label>
+						<input
+							type="checkbox"
+							value={3}
+							name="Accion"
+							onChange={handleCheckboxGenres}
+						/>
+						Accion
+					</label>
+				</div>
+
+				<div>
+					<label>
+						<input
+							type="checkbox"
+							value={1}
+							name="PS5"
+							onChange={handleCheckboxPlatforms}
+						/>
+						PS5
+					</label>
+					<label>
+						<input
+							type="checkbox"
+							value={2}
+							name="Nintendo"
+							onChange={handleCheckboxPlatforms}
+						/>
+						Nintendo
+					</label>
+					<label>
+						<input
+							type="checkbox"
+							value={3}
+							name="Pc"
+							onChange={handleCheckboxPlatforms}
+						/>
+						PC
+					</label>
+				</div>
+				<div>
+					<label>Image: </label>
+					<input
+						type="text"
+						name="background_image"
+						value={background_image}
+						onChange={handleInputChange}
+					/>
+				</div>
+
+				<div>
+					<input type="submit" className="btnSubmit" value="Crear" />
+				</div>
+			</div>
+		</form>
+	);
+};
--- client/src/components/pages/home/newgame/Newgame.js
+++ client/src/components/pages/home/newgame/Newgame.js
@@ -1,3 +1,4 @@
+import { FormNewgame } from '../../../forms/newgame/FormNewgame';
 import { Header } from '../../../layout/header/Header';
 import { Main } from '../../../layout/main/Main';
 
@@ -9,82 +10,7 @@ export const NewGame = () => {
 			<Header />
 			<Main>
 				<div className={styles.formColumn}>
-					<form>
-						<div className={styles.formBox}>
-							<div>
-								<label>Name: </label>
-								<input
-									type="text"
-									name="name"
-									placeholder="Ingrese un nombre"
-								/>
-							</div>
-							<div>
-								<label>Description: </label>
-								<textarea name="description"></textarea>
-							</div>
-							<div>
-								<label>Released: </label>
-								<input type="date" name="released" />
-							</div>
-							<div>
-								<label>Rating: </label>
-								<textarea name="rating"></textarea>
-							</div>
-							<div>
-								<label>
-									<input type="checkbox" value="Aventuras" />
-									Aventuras
-								</label>
-								<label>
-									<input type="checkbox" value="Rol" />
-									Rol
-								</label>
-								<label>
-									<input type="checkbox" value="Accion" />
-									Accion
-								</label>
-								<label>
-									<input type="checkbox" value="Deportes" />
-									Deportes
-								</label>
-								<label>
-									<input type="checkbox" value="Lucha" />
-									Lucha
-								</label>
-							</div>
-							<div>
-								<label>
-									<input type="checkbox" value="PS5" />
-									PS5
-								</label>
-								<label>
-									<input type="checkbox" value="Nintendo" />
-									Nintendo
-								</label>
-								<label>
-									<input type="checkbox" value="Pc" />
-									PC
-								</label>
-								<label>
-									<input type="checkbox" value="Xbox" />
-									Xbox
-								</label>
-								<label>
-									<input type="checkbox" value="PS4" />
-									PS4
-								</label>
-							</div>
-							<div>
-								<label>Image: </label>
-								<input type="text" name="image" />
-							</div>
-
-							<div>
-								<input type="submit" className="btnSubmit" value="Crear" />
-							</div>
-						</div>
-					</form>
+					<FormNewgame />
 				</div>
 			</Main>
 		</div>
